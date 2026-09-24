567c5de00b0ff5db5b72ac4e6386ea5dda88bbc5
Raman Maksimchuk <dotnet044@gmail.com>
2026-09-24 20:54:43 +0300

    Acceptance test for Kestrel server Windows Auth via custom Negotiate lib, but some important DevOps scripting is required!

--- acceptance/Authentication/WindowsAuthTests.cs
+++ acceptance/Authentication/WindowsAuthTests.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.Configuration.File;
 using System.Runtime.InteropServices;
@@ -10,6 +12,11 @@ using _HttpSys_ = Microsoft.AspNetCore.Server.HttpSys;
 
 namespace Ocelot.Acceptance.Authentication;
 
+#pragma warning disable IDE0079 // Remove unnecessary suppression
+#pragma warning disable CA1416 // Validate platform compatibility
+
+[Trait("Feat", "657")] // https://github.com/ThreeMammals/Ocelot/issues/657
+[Trait("PR", "1521")] // https://github.com/ThreeMammals/Ocelot/pull/1521
 public sealed class WindowsAuthTests : Steps
 {
     private const string SuccessfulResposeBody = "Windows Authentication succeeded";
@@ -17,8 +24,6 @@ public sealed class WindowsAuthTests : Steps
     [Theory]
     [InlineData(true, HttpStatusCode.OK, SuccessfulResposeBody)]
     [InlineData(false, HttpStatusCode.Unauthorized, "")]
-    [Trait("Feat", "657")] // https://github.com/ThreeMammals/Ocelot/issues/657
-    [Trait("PR", "1521")] // https://github.com/ThreeMammals/Ocelot/pull/1521
     public async Task ShouldUseDefaultCredentialsForHttpSysServer(bool useCredentials, HttpStatusCode statusCode, string body)
     {
         Assert.SkipUnless(RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
@@ -32,16 +37,65 @@ public sealed class WindowsAuthTests : Steps
         };
         var configuration = GivenConfiguration(route);
 
-        await GivenThereIsAWindowsAuthenticatedServiceRunningOn(port);
+        await handler.GivenThereIsAServiceRunningOnAsync(port, NoConfiguration, NoLogging, HttpSysServices, HttpSysAuthApp, ConfigureHttpSys);
         GivenThereIsAConfiguration(configuration);
         GivenOcelotIsRunning();
         await WhenIGetUrlOnTheApiGateway("/");
         ThenTheStatusCodeShouldBe(statusCode);
         await ThenTheResponseBodyShouldBeAsync(body);
     }
+    private void HttpSysServices(IServiceCollection services) => services
+        .AddAuthentication(_HttpSys_.HttpSysDefaults.AuthenticationScheme);
+    private void WithNegotiateAuthentication(_HttpSys_.HttpSysOptions options)
+    {
+        var auth = options.Authentication;
+        auth.Schemes = _HttpSys_.AuthenticationSchemes.Negotiate;
+        auth.AllowAnonymous = false;
+    }
+    private void HttpSysAuthApp(IApplicationBuilder app) => app
+        .Run(MapWindowsAuthentication);
+    private void ConfigureHttpSys(IWebHostBuilder builder) => builder
+        .UseHttpSys(WithNegotiateAuthentication);
 
-#pragma warning disable IDE0079 // Remove unnecessary suppression
-#pragma warning disable CA1416 // Validate platform compatibility
+    [Theory]
+    [InlineData(true, HttpStatusCode.OK, SuccessfulResposeBody)]
+    [InlineData(false, HttpStatusCode.Unauthorized, "")]
+    public async Task ShouldUseDefaultCredentialsForKestrelServer(bool useCredentials, HttpStatusCode statusCode, string body)
+    {
+        Assert.SkipUnless(RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            $"Testing Windows Authentication with Kestrel is not applicable on the \"{RuntimeInformation.OSDescription}\" platform.");
+
+        var port = PortFinder.GetRandomPort();
+        var route = GivenRoute(port);
+        route.HttpHandlerOptions = new FileHttpHandlerOptions
+        {
+            UseDefaultCredentials = useCredentials,
+        };
+        var configuration = GivenConfiguration(route);
+
+        await handler.GivenThereIsAServiceRunningOnAsync(port, NoConfiguration, NoLogging, KestrelServices, KestrelAuthApp, ConfigureKestrel);
+        GivenThereIsAConfiguration(configuration);
+        GivenOcelotIsRunning();
+        await WhenIGetUrlOnTheApiGateway("/");
+        ThenTheStatusCodeShouldBe(statusCode);
+        await ThenTheResponseBodyShouldBeAsync(body);
+    }
+    private void WithFallbackPolicy(AuthorizationOptions options)
+        => options.FallbackPolicy = new AuthorizationPolicyBuilder(NegotiateDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build();
+    private void KestrelServices(IServiceCollection services)
+    {
+        services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+        services.AddAuthorization(WithFallbackPolicy);
+    }
+    private void ConfigureKestrel(IWebHostBuilder builder)
+        => builder.UseKestrel();
+    private void KestrelAuthApp(IApplicationBuilder app) => app
+        .UseAuthentication()
+        .UseAuthorization()
+        .Run(MapWindowsAuthentication);
+
     private Task MapWindowsAuthentication(HttpContext context)
     {
         var response = context.Response;
@@ -57,25 +111,6 @@ public sealed class WindowsAuthTests : Steps
         response.StatusCode = StatusCodes.Status401Unauthorized;
         return Task.CompletedTask;
     }
-    private void UseNegotiateAuthentication(HttpSysOptions options)
-    {
-        var auth = options.Authentication;
-        auth.Schemes = _HttpSys_.AuthenticationSchemes.Negotiate;
-        auth.AllowAnonymous = false;
-    }
-    private void WindowsAuthApp(IApplicationBuilder app) => app
-        .Run(MapWindowsAuthentication);
-    private void ConfigureHttpSys(IWebHostBuilder builder) => builder
-        .Configure(WindowsAuthApp)
-        .UseHttpSys(UseNegotiateAuthentication);
-
-#if NET10_0_OR_GREATER
-    private Task<IHost>
-#else
-    private Task<IWebHost>
-#endif
-    GivenThereIsAWindowsAuthenticatedServiceRunningOn(int port)
-        => handler.GivenThereIsAServiceRunningOnAsync(port, NoConfiguration, NoLogging, NoServices, NoApplications, ConfigureHttpSys);
+}
 #pragma warning restore CA1416
 #pragma warning restore IDE0079
-}
